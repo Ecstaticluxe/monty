f86abe506aa8e7ffec228cc0c99107bd58913440
Essy <esthercikechukwu@gmail.com>
2023-11-16 17:33:20 +0000

    mandatory

--- add.c
+++ add.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "monty.h"
 /**
  * f_add - adds the top two elements of the stack.
@@ -7,26 +8,37 @@
 */
 void f_add(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	int len = 0, aux;
+    stack_t *h;
+    int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	aux = h->n + h->next->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+    h = *head;
+    while (h)
+    {
+        h = h->next;
+        len++;
+    }
+    if (len < 2)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+    h = *head;
+    if (h->next != NULL)
+    {
+        aux = h->n + h->next->n;
+        h->next->n = aux;
+        *head = h->next;
+        free(h);
+    }
+    else
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
 }
