6e3ca0fd2e7d0aa162d3cf6b9a53e538505a74b8
Essy <esthercikechukwu@gmail.com>
2023-11-18 09:38:32 +0000

    monty

--- monty.h
+++ monty.h
@@ -42,16 +42,34 @@ typedef struct instruction_s
 
 void f_push(stack_t **head, unsigned int counter);
 void print_error_and_exit(unsigned int counter);
-
-int is_valid_integer(const char *str);
 void f_pall(stack_t **head, unsigned int counter);
 void f_pint(stack_t **head, unsigned int counter);
 void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
+
 void f_swap(stack_t **head, unsigned int counter);
 void f_nop(stack_t **head, unsigned int counter)
 
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t getstdin(char **lineptr, int file);
+char  *clean_line(char *content);
 
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
+void free_stack(stack_t *head);
+:q`
+void f_add(stack_t **head, unsigned int counter);
+
+void f_sub(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
+void f_mul(stack_t **head, unsigned int counter);
+void f_mod(stack_t **head, unsigned int counter);
+void f_pchar(stack_t **head, unsigned int counter);
+void f_pstr(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head, unsigned int counter);
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
+void f_queue(stack_t **head, unsigned int counter);
+void f_stack(stack_t **head, unsigned int counter);
 
 
 #endif
